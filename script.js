5b1205e67753828b38d4ecb0f3c34e8bb7fb2714
Jean Simas <jeansimas12@hotmail.com>
2020-04-01 16:12:08 -0300

    The user can now copy the css clicking in the box

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+function change() {
+    const topLeft = document.getElementById('topLeft').value || 0
+    const topRight = document.getElementById('topRight').value || 0
+    const bottomLeft = document.getElementById('bottomLeft').value || 0
+    const bottomRight = document.getElementById('bottomRight').value || 0
 
-function test() {
-    // const doc = document.querySelector('.t')
-    // doc.innerHTML = e.value
-    console.log('test')
+    const divBorder = document.getElementById('div-border')
+    divBorder.style.borderRadius = `${topLeft}px ${topRight}px ${bottomRight}px ${bottomLeft}px`
+
+    const cssTextArea = document.getElementById('css')
+
+    const text = `border-radius: ${divBorder.style.borderRadius};`
+    cssTextArea.innerText = text
+
+    console.log(text.length)
+    cssTextArea.cols = text.length
+
+}
+
+function copyText() {
+    const textArea = document.getElementById('css')
+    textArea.select()
+    document.execCommand('copy')
 }
